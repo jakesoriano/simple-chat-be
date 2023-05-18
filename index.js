75269267fcdd3ef412b3a9a191eeed932bcc992b
jsoriano <johnkarlo.soriano@templeandwebster.com.au>
2023-05-18 17:08:36 +0800

    Initial setup for socket.io

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+
+  socket.on('message',(msg)=>{
+    console.log('message : ' + msg);
+    io.emit('message', msg);
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
